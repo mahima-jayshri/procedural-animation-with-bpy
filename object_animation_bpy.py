b0b0d521e07c76cbfe5acada929860c46c6eab92
Mahima Jayshri <mahimajayashri14@gmail.com>
2025-12-27 10:35:20 +0530

    Add camera and light with keyframe animations
    Added camera and light objects with animation keyframes.

--- object_animation_bpy.py
+++ object_animation_bpy.py
@@ -1,11 +1,10 @@
 import bpy
+# CLEANUP
 for obj in bpy.data.objects:
     if obj.name.startswith(("Cube", "Circle")):
         bpy.data.objects.remove(obj, do_unlink=True)
-
-
-
-
+        
+# CUBE
 bpy.ops.mesh.primitive_cube_add(location=(0,0,0.75))
 cube=bpy.context.active_object
 cube.name="Cube"
@@ -34,7 +33,7 @@ cube.keyframe_insert(data_path="hide_viewport",frame=119)
 cube.keyframe_insert(data_path="hide_viewport",frame=119)
 
 
-
+# CIRCLE (CYLINDER)
 bpy.ops.mesh.primitive_cylinder_add(location=(0,0,5))
 circle=bpy.context.active_object
 circle.name="Circle"
@@ -63,3 +62,34 @@ circle.keyframe_insert("location", frame=120)
 
 circle.location = (5, 0, -5)
 circle.keyframe_insert("location", frame=180)
+
+# CAMERA
+bpy.ops.object.camera_add(location=(0, -12, 4))
+camera = bpy.context.active_object
+camera.name = "Camera"
+
+camera.rotation_euler = (1.2, 0, 0)
+
+bpy.context.scene.camera = camera
+# Camera animation
+camera.keyframe_insert("location", frame=1)
+
+camera.location = (0, -8, 3)
+camera.keyframe_insert("location", frame=120)
+
+camera.location = (2, -6, 2)
+camera.keyframe_insert("location", frame=180)
+# LIGHT
+bpy.ops.object.light_add(type='AREA', location=(0, -5, 6))
+light = bpy.context.active_object
+light.name = "Light"
+
+light.data.energy = 1500
+light.keyframe_insert(data_path="energy", frame=1)
+
+light.location = (2, -3, 6)
+light.keyframe_insert("location", frame=120)
+
+light.data.energy = 800
+light.keyframe_insert(data_path="energy", frame=180)
+
